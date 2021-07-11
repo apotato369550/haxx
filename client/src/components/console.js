e7521f833214c99eca812a182711cdd2871d4260
apotato369550 <johnandresyap510@gmail.com>
2021-07-12 07:38:14 +0800

    Work on components

---
+++ client/src/components/console.js
@@ -0,0 +1,12 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+
+const Console = () => {
+    return (
+        <div>
+            Welcome to console page
+        </div>
+    )
+}
+
+export default console;
---
+++ client/src/components/login.js
@@ -0,0 +1,12 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+
+const Login = () => {
+    return (
+        <div>
+            Welcome to login page
+        </div>
+    )
+}
+
+export default Login;
