038c80c1a9566c1794b53e2b07f7b68bfc69753e
apotato369550 <johnandresyap510@gmail.com>
2021-07-13 07:41:09 +0800

    Add bootstrap and organize pages and components

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,23 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+import './css/neon-glow-theme.css';
 import './App.css';
+import Axios from 'axios';
+import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
+import Login from "./pages/login";
+import Home from "./pages/home";
+import Console from "./pages/console";
+
+// owrk on this
 
 function App() {
   return (
-    <div>Hello world:)</div>
+    <Router>
+      <Switch>
+        <Route exact path="/" component={Home}/>
+        <Route exact path="/login" component={Login}/>
+        <Route exact path="/console" component={Console}/>
+      </Switch>
+    </Router>
   );
 }
 
