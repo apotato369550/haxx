6c987c4b30bac0348c60f3438714dc0663fc00d9
apotato369550 <johnandresyap510@gmail.com>
2021-07-29 18:59:26 +0800

    Fix command entry

--- client/src/components/CommandPrompt.jsx
+++ client/src/components/CommandPrompt.jsx
@@ -3,13 +3,31 @@ import axios from 'axios';
 
 // test this out
 const CommandPrompt = ({ command, setCommand, logs, setLogs }) => {
+    // oh shit there are two handlers
+
+    const commandHandler = (event) => { 
+        console.log("command handler has been evoked");
+        setCommand(event.target.value)
+        return;
+    }
+
     const submitHandler = (event) => {
         // command is not working
         console.log("Submit handler has been clicked " + event.target.value)
         
+        // command is not iterable?
+        // it's because it's empty first
+        // work on this
         event.preventDefault();
-        setCommand("");
-        setLogs([...command]);
+        // ^^ test this
+        // command is not iterableVVV
+        // this works... kinda
+        // it does it w/ individual letters
+        // there is no response from the server... i should re-do this portion??
+        
+        setLogs([...logs, command]);
+        // print it out as a string^^^
+        /*
         axios.post("http://localhost:3001/command", {
             command: command
         }).then((response) => {
@@ -18,13 +36,16 @@ const CommandPrompt = ({ command, setCommand, logs, setLogs }) => {
         }).catch((error) => {
             console.log(error);
         });
+        */
+
+        setCommand("");
         return;
     }
 
     return (
         <div>
             <button type="button" onClick={submitHandler}>Enter</button>
-            <input type="text" onChange={setCommand} />
+            <input type="text" onChange={commandHandler} value={command}/>
         </div>
     )
 }
--- client/src/components/CommandTerminal.jsx
+++ client/src/components/CommandTerminal.jsx
@@ -3,7 +3,7 @@ import axios from 'axios';
 // fix this import and test later
 import Log from '../components/Log';
 
-const CommandTerminal = (command, setCommand, logs, setLogs) => {
+const CommandTerminal = ({ command, setCommand, logs, setLogs }) => {
     // make line component instead of log
     // fix this??
     // test this
